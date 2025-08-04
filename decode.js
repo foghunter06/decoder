663a1c3808434969b92c2b2259e6db4879e7a2c6
foghunter06 <foghunter06@gmail.com>
2025-08-04 14:49:43 +0000

    a

--- decode.js
+++ decode.js
@@ -1,18 +1,44 @@
 const fs = require('fs');
 
+// Tarayıcı ortamını taklit eden sahte nesneler
+global.window = {};
+global.document = {};
+global.navigator = {};
+global.location = {};
+global.localStorage = {
+  getItem: () => null,
+  setItem: () => {},
+  removeItem: () => {}
+};
+global.sessionStorage = {
+  getItem: () => null,
+  setItem: () => {},
+  removeItem: () => {}
+};
+global.XMLHttpRequest = function () {
+  return {
+    open: () => {},
+    send: () => {},
+    setRequestHeader: () => {},
+    readyState: 4,
+    status: 200,
+    responseText: ''
+  };
+};
+
 let raw = fs.readFileSync('game.js', 'utf8');
 global.arguments = [];
 
 const log = console.log;
 let output = '';
-console.log = (msg) => { output += msg + '\n'; };
+console.log = (msg) => { output += msg + '\\n'; };
 
 try {
-  eval(raw);
+  eval(raw);  // game.js içeriğini çalıştır
 } catch (e) {
   console.error("Çalıştırma hatası:", e.message);
 }
 
 console.log = log;
-console.log("\n🎯 Decode Edilen Kod:\n");
+console.log("\\n🎯 Decode Edilen Kod:\\n");
 console.log(output);
